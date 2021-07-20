0b837c0baa9e950a50b94f3eb4fa02c83606aabb
Vert_Romance <83347243+VertRomance@users.noreply.github.com>
2021-07-20 17:03:56 +0200

    Delete public directory

--- public/form/notification.php
+++
@@ -1,5 +0,0 @@
-    <?php
-    $retour = mail('cbujalance69@gmail.com', 'Demande de contact', $_POST['message'], 'From : webmaster@moncv.fr');
-    if ($retour) {
-        echo '<p>Votre message a bien été envoyé.</p>';
-    }
